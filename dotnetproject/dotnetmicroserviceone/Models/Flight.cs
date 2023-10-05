bff77cfc5fe70bd59840c8c027442dffaa16962b
Iamneo <techadmin@iamneo.ai>
2023-10-05 17:20:53 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Models/Flight.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetmicroserviceone.Models
+{
+    public class Flight
+    {
+        public int FlightID { get; set;}
+        public string FlightName { get; set;}
+        public decimal TicketPrice {get; set;}
+        public int TotalSeats { get; set;}
+        public string Airline { grt; set;}
+        
+    }
+}
