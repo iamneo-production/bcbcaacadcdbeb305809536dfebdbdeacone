0334d026999940292dda69ad701a3b40bdeecc38
Iamneo <techadmin@iamneo.ai>
2023-10-05 17:30:53 +0000

    Backup Commit

---
+++ dotnetproject/dotnetmicroserviceone/Controllers/FlightController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace dotnetmicroserviceone.Controllers
+{
+    [Route("[controller]")]
+    public class FlightController : Controller
+    {
+        private readonly FlightDbContext _context;
+        public FlightController(FlightDbContext context)
+        {
+           _context = context; 
+        }
+        private readonly ILogger<FlightController> _logger;
+
+        public FlightController(ILogger<FlightController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FlightController>>> GetAllFlights()
+    }
+}
